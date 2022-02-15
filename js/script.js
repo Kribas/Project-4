a109cf54065162560a1747d05c64cf08db533685
Kribas Rimal <32868850+Kribas@users.noreply.github.com>
2022-02-15 18:48:11 +0545

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 const portfolioLightbox = GLightbox({
     selector: '.portfolio-lightbox'
   });
+
+  $(window).on("load", function() {
+    /*==========  Progress Bar  ==========*/
+		if ($('.experience__area-skill-bar').length) {
+			$('.experience__area-skill-bar').appear(function () {
+				var el = $(this);
+				var percent = el.data('width');
+				$(el).css('width', percent + '%');
+			}, {
+				accY: 0
+			});
+		};
+  })
